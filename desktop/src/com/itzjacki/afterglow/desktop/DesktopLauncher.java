171e47ffad7ea1b75448de8a4eae9339e80486fe
Jakob <jakob-ek@hotmail.com>
2020-04-28 21:00:34 +0200

    Initial libGDX setup

---
+++ desktop/src/com/itzjacki/afterglow/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.itzjacki.afterglow.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.itzjacki.afterglow.AfterglowGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AfterglowGame(), config);
+	}
+}
